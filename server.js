300df443cca31f4e078e1425dcab1871c0e25e66
ThuyDoan0112 <doanthuy896@gmail.com>
2023-06-10 12:20:43 +0700

    :technologist: Set up dev environment (#14)

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
 // Require third-party module express
 const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+
+// To read environment variables from .env file
+dotenv.config();
 
 // Init express app
 const app = express();
 
+// Middlewares
+app.use((req, res, next) => {
+  console.log('Time: ', Date.now());
+  next();
+});
+
+app.use(morgan('dev'));
+
 // Handle routes
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.send('Hello, World');
 });
 
 app.get('/users', (req, res) => {
@@ -25,7 +38,7 @@ app.get('/users', (req, res) => {
 });
 
 // Start express server
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 });
